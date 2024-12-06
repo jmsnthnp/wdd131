1aea2dd72e5e6c1a5e37653407917e9ec1518844
jmsnthnp <jmsnthnp@gmail.com>
2024-12-06 08:54:09 +0800

    update

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,9 +1,21 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const navMenu = document.querySelector("nav ul");
 
-  // Hamburger menu functionality
-  hamburger.addEventListener("click", () => {
-    if (navMenu.style.display === "block") {navMenu.style.display = "none";hamburger.textContent = "☰";} 
-    else {navMenu.style.display = "block";hamburger.textContent = "✖";}});
+document.addEventListener('DOMContentLoaded', () => {
+  const hamButton = document.querySelector('#menu');
+  const navigation = document.querySelector('.navigation');
+  const nameHeader = document.querySelector('.nameHeader');
+
+  hamButton.addEventListener('click', () => {
+      navigation.classList.toggle('open');
+      hamButton.classList.toggle('open');
+      nameHeader.classList.toggle('hidden');});
 });
+
+
+
+// Footer information
+const currentYear = new Date().getFullYear();
+const lastModified = document.lastModified;
+const copyrightYearElement = document.getElementById('currentyear');
+const lastModifiedElement = document.getElementById('lastModified');
+copyrightYearElement.textContent = currentYear;
+lastModifiedElement.textContent = `Last update: ${lastModified}`;
